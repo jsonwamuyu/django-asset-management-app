8aabb48a99e2e52f4de2ccff4f8f59670bc7a499
jsonwamuyu <jysonmuchiri@gmail.com>
2025-06-06 07:37:37 +0300

    Setup middleware

--- backend/assets/custom_middlewares/asset_management.py
+++ backend/assets/custom_middlewares/asset_management.py
@@ -9,7 +9,15 @@ class AssetManagementMiddleware:
         logger.info("AssetManagementMiddleware initialized.")
 
     def __call__(self, request):
+        user = request.user
+        path = request.path
+
+        if user.is_authenticated:
+            logger.info(f"User {user.username} Role: {user.role} accessed {path}")
 
         response = self.get_response(request)
 
+        if user.is_authenticated:
+            logging.info(f"Response sent to {user.username} for {path}")
+            
         return response
