2191ca9a9f80d9aebc19a4d17c2f73b881b6479b
jsonwamuyu <jysonmuchiri@gmail.com>
2025-06-06 10:30:36 +0300

    Paginate posts

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,10 +1,47 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 # Create your views here.
 
+Posts = [
+    {
+        'id':1,
+        'title':'Make it happen', 
+        'body':"Think of this as a way to make everything work out as expected"
+    },
+    {
+        'id':2,
+        'title':'Make it happen', 
+        'body':"Think of this as a way to make everything work out as expected"
+    },
+    {
+        'id':3,
+        'title':'Make it happen', 
+        'body':"Think of this as a way to make everything work out as expected"
+    },
+    {
+        'id':4,
+        'title':'Make it happen', 
+        'body':"Think of this as a way to make everything work out as expected"
+    },
+    {
+        'id':5,
+        'title':'Make it happen', 
+        'body':"Think of this as a way to make everything work out as expected"
+    },
+    {
+        'id':6,
+        'title':'Make it happen', 
+        'body':"Think of this as a way to make everything work out as expected"
+    }
+]
+
 def posts(request):
     # return HttpResponse('posts page')
-    posts = Post.objects.all()
-    return render(request, 'posts/posts.html', {'posts':posts})
+    # posts = Post.objects.all().order_by('-id')
+    paginator = Paginator(Posts, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'posts/posts.html', {'page_obj':page_obj})
