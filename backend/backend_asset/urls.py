f76341ab749b9123aeec3ecbc48c8bab273759cd
jsonwamuyu <jysonmuchiri@gmail.com>
2025-06-07 17:51:15 +0300

    Install Pillow for images

--- backend/backend_asset/urls.py
+++ backend/backend_asset/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,3 +16,6 @@ urlpatterns = [
     # API routes from the assets app
     path("api/",include("assets.urls",namespace='assets')),
 ]
+
+# Tells django where to find images to use inside app
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
